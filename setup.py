c5fa08a19888a8ee62df08329f3ea5d4532a66ca
Paul Dodd <pdodd@umich.edu>
2017-04-01 00:13:04 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+import sys
+from setuptools import setup, find_packages
+
+setup(
+    name='pynomial',
+    version='0.0.0',
+    packages=find_packages(),
+    author='Paul M Dodd',
+    author_email='pdodd@umich.edu',
+    description="python package for combinatorial problems",
+    url="https://github.com/PaulDodd/pynomial.git",
+    install_requires=[], #  install_requires or something else? 
+)
