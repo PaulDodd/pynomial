dd8d625eda31075ed9092a4553e6078bb640711d
Paul Dodd <pdodd@umich.edu>
2017-06-29 12:24:37 -0400

    implemented GED needs testing

--- pynomial/graph/NN.h
+++ pynomial/graph/NN.h
@@ -3,72 +3,183 @@
 
 #include "Network.h"
 #include <pynomial/extern/nanoflann/include/nanoflann.hpp>
+#include "../register/brute_force.h"
 namespace pynomial{
 namespace graph{
 
 template<typename T>
-class GraphDistance
+struct edit_path{
+    // Some constanst to store some state information.
+    static const size_t null;//     = -1; see below.
+    static const size_t notset;//   = -2;
+    // member varaibles
+    std::vector<size_t> forward; // will have g1.NumNodes()
+    std::vector<size_t> reverse; // will have g2.NumNodes()
+    T cost;
+    // some helper methods
+    edit_path(size_t n1, size_t n2, const T& c) : forward(n1, notset), reverse(n2, notset), cost(c) {}
+
+    edit_path(const edit_path<T>& other) : forward(other.forward), reverse(other.reverse), cost(other.cost) {}
+
+    edit_path(edit_path<T>&& other) : forward(std::move(other.forward)), reverse(std::move(other.reverse)), cost(other.cost) {}
+
+    bool operator < (const edit_path<T>& other){ return cost < other.cost; }
+
+    bool operator > (const edit_path<T>& other){ return cost > other.cost; }
+
+    size_t& operator[](const size_t& i){ return forward[i]; }
+
+    size_t& operator()(const size_t& i){ return reverse[i]; }
+
+    bool complete() const
+    {
+        for(size_t val : forward)
+            if(val == notset)
+                return false;
+        for(size_t val : reverse)
+            if(val == notset)
+                return false;
+        return true;
+    }
+
+    size_t count_mapped() const
+    {
+        return std::count(forward.begin(), forward.end(), notset);
+    }
+
+    edit_path<T>& flip()
+    {
+        std::vector<size_t> f = forward;
+        forward = reverse;
+        reverse = f;
+        return *this;
+    }
+
+};
+template< typename T>
+const size_t edit_path<T>::null = -1;
+template<typename T>
+const size_t edit_path<T>::notset = -2;
+
+
+template<typename T>
+class GraphEditDistance
 {
 public:
-    GraphDistance() {}
-    T operator()(const CNetwork& g1, const CNetwork& g2)
+    typedef edit_path<T> edit_path_t;
+    typedef std::function<T(const edit_path_t&, const CNetwork&, const CNetwork&)> CostFunction;
+private:
+        CostFunction    m_Cost;
+        CostFunction    m_Heuristic;
+public:
+    GraphEditDistance() {}
+
+    edit_path_t operator()(const CNetwork& g1, const CNetwork& g2)
     {
-        T dist = 10.0;
-        const CNetwork* pg1 = &g1, *pg2 = &g2;
-        if(pg1->NumNodes() < pg2->NumNodes())
+        if(g1.NumNodes() > g2.NumNodes())
         {
-            std::swap(pg1, pg2);
+            return (*this)(g2, g1).flip();
         }
-
-
-        CIsomorphism<> iso(pg1,pg2); // g1 -> g2
-        iso.ComputeMatching();
-        std::cout << "g1 = " << g1.NumNodes() << ", g2 = " << g2.NumNodes() << ", mappings = " << iso.NumMappings() << std::endl;
-
-        for(CIsomorphism<>::iso_iterator it = iso.begin(); it != iso.end(); it++)
+        std::set< edit_path_t > open;
+        for(size_t i = 0; g2.NumNodes(); i++)
         {
-            T d = 0.0;
-            for(CNetwork::edge_iterator edge(pg1); edge.IsValid(); edge++) // TODO: think about this more and make sure it is logically correct.
+            edit_path_t path(g1.NumNodes(), g2.NumNodes(), 0.0);
+            path[0] = i; // set the forward and reverse maps.
+            path(i) = 0;
+            path.cost = m_Cost(path, g1, g2);
+            open.insert(path);
+        }
+        edit_path_t path(g1.NumNodes(), g2.NumNodes(), 0.0);
+        path[0] = edit_path_t::null; // deletion of the node.
+        path.cost = m_Cost(path, g1, g2);
+        open.insert(path);
+        edit_path_t min_path;
+        size_t n = g1.NumNodes();
+        while(true)
+        {
+            if(open.begin() == open.end())
             {
-                size_t src = it->at(edge->source), tgt = it->at(edge->target);
-                if(CIsomorphism<>::is_null(src) || CIsomorphism<>::is_null(tgt))
+                std::cout << "ERROR!! Could not find a minimum edit path!" << std::endl;
+                throw(std::runtime_error("ERROR!! Could not find a minimum edit path!"));
+            }
+            min_path = *open.begin();
+            open.erase(open.begin());
+            if( min_path.complete() ) // we found it!
+            {
+                break;
+            }
+            size_t k = min_path.count_mapped();
+            if(k < n)
+            {
+                for(size_t v2 = 0; v2 < min_path.reverse.size(); v2++)
                 {
-                    std::cout << "w = " << edge->weight << std::endl;
-                    d += edge->weight;
+                    if(min_path.reverse[v2] == edit_path_t::notset) // we have not mapped this one yet.
+                    {
+                        edit_path_t new_path(min_path);
+                        new_path[k] = v2;
+                        new_path(v2) = k;
+                        new_path.cost = m_Cost(new_path, g1, g2);
+                        open.insert(std::move(new_path)); // use move semantics to avoid another copy.
+                    }
                 }
-                else{
-                    double w = pg2->GetAdjMatrix().coeff(tgt, src);
-                    std::cout << "diff = " << fabs(edge->weight - w) << std::endl;
-                    d += fabs(edge->weight - w);
+                edit_path_t new_path(min_path);
+                new_path[k] = edit_path_t::null; // delete the node.
+                new_path.cost = m_Cost(new_path, g1, g2);
+                open.insert(std::move(new_path)); // use move semantics to avoid another copy.
+            }
+            else
+            {
+                // Here we have mapped all nodes in g1 -> g2
+                // but there are some nodes still in g2 that must be deleted.
+                edit_path_t new_path(min_path);
+                for(size_t v2 = 0; v2 < min_path.reverse.size(); v2++)
+                {
+                    if(min_path.reverse[v2] == edit_path_t::notset)
+                    {
+                        new_path(v2) = edit_path_t::null; // delete the nodes.
+                    }
                 }
-
-            }
-            // for(size_t i = 0; i < it->size(); i++)
-            // {
-            //     if(CIsomorphism<>::is_null(it->at(i)))
-            //     {
-            //         std::cout << "null" << ", ";
-            //     }
-            //     else
-            //     {
-            //         std::cout << it->at(i) << ", ";
-            //     }
-            // }
-            // std::cout <<" d = "<< d << std::endl;
-            if(it == iso.begin())
-            {
-                dist = d;
-                // mapping = *it;
-            }
-            else if (dist > d)
-            {
-                dist = d;
-                // mapping = *it; // potentially doing many copies.
+                assert(new_path.complete()); // now all nodes will be mapped.
+                new_path.cost = m_Cost(new_path, g1, g2);
+                open.insert(std::move(new_path));
             }
         }
-        return dist;
+        return min_path;
     }
-    std::vector<size_t> mapping;
+
+    void SetCost( CostFunction func ) { m_Cost = func; }
+
+    void SetHeuristic( CostFunction func ) { m_Heuristic = func; }
+};
+
+class CostFunction {
+/*
+Some notes and properties that should be held by the cost functions.
+
+
+1.  c(e) >= 0, for all node and edge edit operations e.
+2.  c(e) > 0, for all node and edge deletions and insertions e.
+3.  c(u -> w) <= c(u -> v) + c(v -> w)
+    c(u -> ε) <= c(u -> v) + c(v -> 0)
+    c(0 -> v) <= c(0 -> u) + c(u -> v)
+
+2 and 3 make sure we dont make superfluous substitutions.
+
+*/
+};
+
+template< typename Label>
+class MinkowskiCost : CostFunction
+{
+    double m_tau;
+public:
+    MinkowskiCost(double tau) : m_tau(tau) {}
+    double operator ()(const GraphEditDistance<double>::edit_path_t& path, const CNetwork& g1, const CNetwork& g2)
+    {
+        double cost = 0.0;
+
+        return cost;
+    }
 };
 
 class NN
@@ -132,53 +243,56 @@ public:
     void compute();
     void match()
     {
-        if(m_npoints <=1) throw std::runtime_error("matching requires more than one point");
-        m_dist.clear();
-        m_dist.reserve(m_npoints*(m_npoints-1)/2);
-        GraphDistance<double> d;
-        for(size_t i = 0; i < m_npoints-1; i++)
-        {
-            CNetwork gi;
-            gi.AddNetworkType(m_network.GetNetworkMask());
-            vector<size_t> ni = m_network.GetNeighbors(i);
 
-            ni.push_back(i);
-            std::sort(ni.begin(), ni.end());
+        //
 
-            // for(size_t k = 0; k < ni.size(); k++)
-            // {
-            //     std::cout << "w("<< i << ", "<< ni[k]<< ")="<<m_network.GetAdjMatrix().coeff(ni[k],i) << std::endl;
-            // }
-            m_network.SubGraphEx(ni.begin(), ni.end(), gi);
-            // m_network.SubGraph(ni.begin(), ni.end(), gc);
-            std::cout << "**************** SubGraph "<< i <<" ************* " << std::endl;
-            gi.PrintNetwork();
-            // std::cout << "**************** check ************* " << std::endl;
-            // gc.PrintNetwork();
-            std::cout << "processing particle " << i << " with "<< ni.size() << " neighbors" <<'\r' << std::flush;
-            // std::cout << "processing particle " << i << '\r' << std::flush;
-            for(size_t j = i+1; j < m_npoints; j++)
-            {
-                // break;
-                CNetwork gj;
-                gj.AddNetworkType(m_network.GetNetworkMask());
-                vector<size_t> nj = m_network.GetNeighbors(j);
-                nj.push_back(j);
-                std::sort(nj.begin(), nj.end());
-                m_network.SubGraphEx(nj.begin(), nj.end(), gj);
-                std::cout << "**************** SubGraph "<< j <<" ************* " << std::endl;
-                gj.PrintNetwork();
-
-                double dist = d(gi,gj);
-                std::cout << "processing particle " << i << " with "<< j << " dist = "<< dist <<'\r' << std::endl;
-                m_dist.push_back(dist);
-                if(j>i+3) return;
-                // return;
-            }
-            // if(i > 1)
-            //     break;
-        }
-        std::cout << std::endl;
+        // if(m_npoints <=1) throw std::runtime_error("matching requires more than one point");
+        // m_dist.clear();
+        // m_dist.reserve(m_npoints*(m_npoints-1)/2);
+        // GraphDistance<double> d;
+        // for(size_t i = 0; i < m_npoints-1; i++)
+        // {
+        //     CNetwork gi;
+        //     gi.AddNetworkType(m_network.GetNetworkMask());
+        //     vector<size_t> ni = m_network.GetNeighbors(i);
+        //
+        //     ni.push_back(i);
+        //     std::sort(ni.begin(), ni.end());
+        //
+        //     // for(size_t k = 0; k < ni.size(); k++)
+        //     // {
+        //     //     std::cout << "w("<< i << ", "<< ni[k]<< ")="<<m_network.GetAdjMatrix().coeff(ni[k],i) << std::endl;
+        //     // }
+        //     m_network.SubGraphEx(ni.begin(), ni.end(), gi);
+        //     // m_network.SubGraph(ni.begin(), ni.end(), gc);
+        //     std::cout << "**************** SubGraph "<< i <<" ************* " << std::endl;
+        //     gi.PrintNetwork();
+        //     // std::cout << "**************** check ************* " << std::endl;
+        //     // gc.PrintNetwork();
+        //     std::cout << "processing particle " << i << " with "<< ni.size() << " neighbors" <<'\r' << std::flush;
+        //     // std::cout << "processing particle " << i << '\r' << std::flush;
+        //     for(size_t j = i+1; j < m_npoints; j++)
+        //     {
+        //         // break;
+        //         CNetwork gj;
+        //         gj.AddNetworkType(m_network.GetNetworkMask());
+        //         vector<size_t> nj = m_network.GetNeighbors(j);
+        //         nj.push_back(j);
+        //         std::sort(nj.begin(), nj.end());
+        //         m_network.SubGraphEx(nj.begin(), nj.end(), gj);
+        //         std::cout << "**************** SubGraph "<< j <<" ************* " << std::endl;
+        //         gj.PrintNetwork();
+        //
+        //         double dist = d(gi,gj);
+        //         std::cout << "processing particle " << i << " with "<< j << " dist = "<< dist <<'\r' << std::endl;
+        //         m_dist.push_back(dist);
+        //         if(j>i+3) return;
+        //         // return;
+        //     }
+        //     // if(i > 1)
+        //     //     break;
+        // }
+        // std::cout << std::endl;
     }
     size_t num_points() {return m_npoints; }
     const std::vector<double>& pair_distance() {return m_dist; }
