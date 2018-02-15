1d4b7983011d10b130b10f08988030059c80f520
Paul Dodd <pdodd@umich.edu>
2018-02-15 16:44:00 -0500

    basic test for similarity measure

--- pynomial/polyhedron/test/test_minkowski.py
+++ pynomial/polyhedron/test/test_minkowski.py
@@ -2,14 +2,24 @@
 
 import itertools as it
 import pynomial as pn
-
 import numpy as np
+
+
 verts = []
 for x,y,z in it.product([-1,1], [-1,1], [-1,1]):
     verts.append([x,y,z]);
 verts = np.array(verts)/2.0;
 print(verts)
+np.random.seed(592765);
+dt = np.random.normal(0, 0.025, verts.shape);
+print(dt)
+verts2 = verts+dt;
 
 p = pn.polyhedron.Polyhedron(verts)
+q = pn.polyhedron.Polyhedron(verts2)
 p.merge_facets();
+q.merge_facets();
 sdr = pn.polyhedron._polyhedron._SDR(p)
+sdr2 = pn.polyhedron._polyhedron._SDR(q)
+
+pn.polyhedron._polyhedron.SimilarityMeasure(sdr, sdr2)
